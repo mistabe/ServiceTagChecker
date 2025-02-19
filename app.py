935d407e0fe6a82bd60bbba0bde5cf6bbf0d54f4
Paul Beyer <gitconfig.charcoal218@passmail.net>
2025-02-19 08:42:49 +0000

    Geting the Flask App to respond with data

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from servicetags import resolve_ip
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+        result = resolve_ip(user_input)
+    return render_template("index.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(degug=True)
---
+++ httperrorhandling.py
@@ -0,0 +1,64 @@
+def API_Read_Safe(url, timeout=5, attempts=3):
+    """Read API URL, handle HTTP 429 and timeout errors"""
+
+    tries = 0
+
+    while tries < attempts:
+
+        tries += 1
+
+        try:
+
+            # read target URL
+
+            response = requests.get(url, headers=headers, timeout=timeout)
+
+            # if successful, return the response
+
+            if response.ok:
+
+                print("Completed successfully")
+
+                return response
+
+            # If response is not "ok", handle error code 429:
+
+            # - wait "Retry-After" seconds or 1 sec, if not present
+
+            # - retry
+
+            if response.status_code == 429:
+
+                try:
+
+                    retry_after = int(response.headers.get("Retry-After"))
+
+                except Exception:
+
+                    retry_after = 1
+
+                print(f"Retry after {retry_after} seconds")
+
+                sleep(retry_after)
+
+                continue
+
+            # if not OK and not 429, then it's something unrecoverable
+
+            print(f"HTTP error: {response.status_code}")
+
+            response.raise_for_status()
+
+        ## if timeout error, retry. If any other error, leave it unhandled
+
+        except requests.exceptions.ConnectTimeout:
+
+            print("Timeout, retry")
+
+            continue
+
+    # return "None" if unsuccessful after all attempts
+
+    print("Unable to get any response")
+
+    return None
