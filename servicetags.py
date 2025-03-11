7e1b2caf532e925057ed087099744c158134305a
Paul Beyer <gitconfig.charcoal218@passmail.net>
2025-03-11 11:49:24 +0000

    Updated servicetags.py
    resolve_ip function so that output is legible from the WebUI.
    Reached MVP for the container to be published

--- servicetags.py
+++ servicetags.py
@@ -76,14 +76,15 @@ def resolve_ip(flask_query):
     jsonfiledate()
     servicetags = loadjson()
     matches = []
+    
     for tag in servicetags["values"]:
-        # print(tag["properties"])
         for addressprefix in tag["properties"]["addressPrefixes"]:
             if is_in_prefix(flask_query, addressprefix):
-                hit = (f"in the ServiceTag:", tag["name"])
-                hit = str(hit)
-                matches.extend(hit)
+                hit = f"in the ServiceTag: {tag['name']}"
+                matches.append(hit)
+
     return matches
+
 
 
 def main():
